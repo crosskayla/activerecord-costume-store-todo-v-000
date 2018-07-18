c035b462311f250a64322b26730c8e9a030bad24
Kayla Cross <kac288@georgetown.edu>
2018-07-18 18:26:58 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+
+class HauntedHouse < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+
+end
