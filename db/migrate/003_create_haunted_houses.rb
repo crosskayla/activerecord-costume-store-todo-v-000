e7fe342c15c474a102b89f29158f90067c7b84a5
Kayla Cross <kac288@georgetown.edu>
2018-07-18 18:31:37 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.
       t.timestamps
     end
   end
