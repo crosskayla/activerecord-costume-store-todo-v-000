976b08e2bfd7013256d503530b2cae5e3d23ca47
Kayla Cross <kac288@georgetown.edu>
2018-07-18 18:29:14 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.
       t.timestamps
     end
   end
