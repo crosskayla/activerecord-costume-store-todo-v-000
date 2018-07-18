0746aa90d9b933652790847dc537c37c98def6c9
Kayla Cross <kac288@georgetown.edu>
2018-07-18 18:27:34 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,9 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.float :price
-      t.string :image_url
-      t.string :size
+      t.string :location
       t.timestamps
     end
   end
